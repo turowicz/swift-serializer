3fa336d162e6edce5798bb49602e6f2cd0443533
Brian Anderson <banderson@solstice-mobile.com>
2015-07-29 16:24:54 -0500

    - Ran Swift 2.0 conversion - Updated the reflect() method to use the Mirror(reflecting:) initializer

--- src/Serializable.swift
+++ src/Serializable.swift
@@ -1,31 +1,31 @@
 /*
 
-    Converts A class to a dictionary, used for serializing dictionaries to JSON
+Converts A class to a dictionary, used for serializing dictionaries to JSON
 
-    Supported objects:
-    - Serializable derived classes (sub classes)
-    - Arrays of Serializable
-    - NSData
-    - String, Numeric, and all other NSJSONSerialization supported objects
+Supported objects:
+- Serializable derived classes (sub classes)
+- Arrays of Serializable
+- NSData
+- String, Numeric, and all other NSJSONSerialization supported objects
 
 */
 
 import Foundation
 
 public class Serializable: NSObject {
-    
+
     /**
-        Converts the class to a dictionary.
-    
-        :returns: The class as an NSDictionary.
+    Converts the class to a dictionary.
+
+    - returns: The class as an NSDictionary.
     */
     public func toDictionary() -> NSDictionary {
         let propertiesDictionary = NSMutableDictionary()
-        let mirror = reflect(self)
-        
-        for i in 1..<mirror.count {
-            let (propName, childMirror) = mirror[i]
-            
+        let mirror = Mirror(reflecting: self)
+
+        for childMirror in mirror.children {
+            let propName = childMirror.label!
+
             if let propValue: AnyObject = self.unwrap(childMirror.value) as? AnyObject {
                 if let serializablePropValue = propValue as? Serializable {
                     propertiesDictionary.setValue(serializablePropValue.toDictionary(), forKey: propName)
@@ -34,7 +34,7 @@ public class Serializable: NSObject {
                     for item in arrayPropValue {
                         subArray.append(item.toDictionary())
                     }
-                    
+
                     propertiesDictionary.setValue(subArray, forKey: propName)
                 } else if propValue is Int || propValue is Double || propValue is Float {
                     propertiesDictionary.setValue(propValue, forKey: propName)
@@ -47,66 +47,61 @@ public class Serializable: NSObject {
                 }
             }
         }
-        
+
         return propertiesDictionary
     }
-    
+
     /**
-        Converts the class to JSON.
-    
-        :returns: The class as JSON, wrapped in NSData.
+    Converts the class to JSON.
+
+    - returns: The class as JSON, wrapped in NSData.
     */
     public func toJson(prettyPrinted : Bool = false) -> NSData? {
         let dictionary = self.toDictionary()
-        
+
         if NSJSONSerialization.isValidJSONObject(dictionary) {
             do {
                 let json = try NSJSONSerialization.dataWithJSONObject(dictionary, options: (prettyPrinted ? .PrettyPrinted : NSJSONWritingOptions()))
                 return json
-            } catch {
-                //currently swift will not catch NSInvalidArgumentException exception
-                print("ERROR: Unable to serialize json, error: \(error)")
+            } catch let error as NSError {
+                print("ERROR: Unable to serialize json, error: \(error)", appendNewline: true)
                 NSNotificationCenter.defaultCenter().postNotificationName("CrashlyticsLogNotification", object: self, userInfo: ["string": "unable to serialize json, error: \(error)"])
             }
         }
-        
+
         return nil
     }
-    
+
     /**
-        Converts the class to a JSON string.
-    
-        :returns: The class as a JSON string.
+    Converts the class to a JSON string.
+
+    - returns: The class as a JSON string.
     */
     public func toJsonString(prettyPrinted : Bool = false) -> String? {
         if let jsonData = self.toJson(prettyPrinted) {
             return NSString(data: jsonData, encoding: NSUTF8StringEncoding) as String?
         }
-        
+
         return nil
     }
 }
 
 extension Serializable {
-    
+
     /**
-        Unwraps 'any' object. See http://stackoverflow.com/questions/27989094/how-to-unwrap-an-optional-value-from-any-type
-    
-        :returns: The unwrapped object.
+    Unwraps 'any' object. See http://stackoverflow.com/questions/27989094/how-to-unwrap-an-optional-value-from-any-type
+
+    - returns: The unwrapped object.
     */
     private func unwrap(any: Any) -> Any? {
-        let mi = reflect(any)
-        
-        if mi.disposition != .Optional {
-            return any
-        }
-        
-        if mi.count == 0 {
+        let mi = Mirror(reflecting: any)
+
+        if mi.children.count == 0 {
             return nil
         }
-        
-        let (_, some) = mi[0]
-        
-        return some.value
+
+        let (_, some) = mi.children.first!
+
+        return some
     }
 }
